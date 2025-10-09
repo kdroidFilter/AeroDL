3ec52ea55bfe0429336f55fe6e8630df165da830
Elie Gambache <elyahou.hadass@gmail.com>
2025-10-09 19:29:54 +0300

    Update conf and logo

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -86,12 +86,6 @@ kotlin {
     }
 }
 
-dependencies {
-    linuxAmd64(compose.desktop.linux_x64)
-    macAmd64(compose.desktop.macos_x64)
-    macAarch64(compose.desktop.macos_arm64)
-    windowsAmd64(compose.desktop.windows_x64)
-}
 
 
 compose.desktop {
